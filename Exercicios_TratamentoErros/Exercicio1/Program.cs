cad0e961314fa0eba3494f6c5b30d075df99fede
bezerrafernando <fernandodbezerra@gmail.com>
2023-08-15 18:46:27 -0300

    Learn

---
+++ Exercicios_TratamentoErros/Exercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Escreva um program onde o usuário é solicitado a informar um valor via teclado e armazenar o valor na
+variável entrada do tipo string onde tem que verificar 3 condições diferentes:
+a- Se a entrada é nula, uma exceção NullReferenceException é lançada com a mensagem "A entrada não pode ser
+nula."
+b-Se a entrada está vazia, uma exceção ArgumentException é lançada com a mensagem "A entrada não pode estar
+vazia."
+c-Se a entrada passar nas verificações anteriores, a entrada é exibida na tela.
+Realize o tratamento de erro para essas condições usando o bloco try-cath-finally */
+
+try
+{
+    string input = Console.ReadLine();
+    if (input == null)
+    {
+        throw new NullReferenceException("A entranda não pode ser nula.");
+    }
+    if (input.Length == 0)
+    {
+        throw new ArgumentException("A entrada não pode estar vazia.");
+    }
+    Console.WriteLine("A entrada é: " + input);
+}
+catch (NullReferenceException e)
+{
+    Console.WriteLine("Erro:" + e.Message);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Erro inesperado" + e.Message);
+}
+finally
+{
+    Console.WriteLine("O programa foi concluído.");
+}
---
+++ Exercicios_TratamentoErros/Exercicio2/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Escreva um programa que solicite ao usuário a informação da idade e do nome via teclado que deverão ser
+armazenados nas variáveis idade do tipo int e nome do tipo string.
+
+A seguir realize o tratamento de erro e lançando as exceções considerando as seguintes condições:
+
+a- Se a idade é negativa, uma exceção ArgumentException é lançada com a mensagem "A idade não pode ser
+negativa."
+
+b - Se a idade é zero, uma exceção NotImplementedException é lançada com a mensagem "A idade ainda não foi
+definida."
+
+c- Se o nome é nulo, uma exceção NullReferenceException é lançada com a mensagem "O nome não pode ser nulo." */
+int idade = 0;
+string nome = null;
+try
+{
+    Console.WriteLine("Informe a sua idade");
+    idade = Convert.ToInt32(Console.ReadLine());
+    if (idade < 0)
+
+    {
+        throw new ArgumentException("A idade não pode ser negativa.");
+    }
+    if (idade == 0)
+    {
+        throw new NotImplementedException("A idade ainda não foi definida.");
+    }
+    Console.WriteLine("Informe o seu nome");
+    nome = Console.ReadLine();
+    if (nome == null)
+    {
+        throw new NullReferenceException("O nome não pode ser nulo.");
+    }
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Erro de argumento: " + e.Message);
+}
+catch (NotImplementedException e)
+{
+    Console.WriteLine("Erro de implementação: " + e.Message);
+}
+catch (NullReferenceException e)
+{
+    Console.WriteLine("Erro de referência nula: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Erro inesperado: " + e.Message);
+}
