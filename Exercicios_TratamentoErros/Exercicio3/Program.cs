56459f106a9922e0fa16d11b8636356c475eb68c
bezerrafernando <fernandodbezerra@gmail.com>
2023-08-15 21:26:14 -0300

    Learn

---
+++ Exercicios_TratamentoErros/Exercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Dado um array de inteiros expresso da seguinte forma
+int[] numeros = new int[] { 109, 211, 313, 405, 519, 617, 711, 891, 951, 1001 };
+Exiba na janela do console os numeros do array e solicite via teclado ao usuário para informar o valor de um índice
+do array para obter o seu respectivo valor.
+Realize o tratamento de exceções filtrando as exceções IndexOutOfRangeException e ArgumentNullException */
+
+try
+{
+    int[] numeros = new int[] { 109, 211, 313, 405, 519, 617, 711, 891, 951, 1001 };
+    int index = 99;
+
+    Console.WriteLine("Dado um array de número inteiros contendo os seguintes valores:");
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        Console.Write(numeros[i] + " ");
+    }
+    Console.WriteLine("\n\nInforme o índece do arry para obter um valor:");
+    index = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\nO número na posição" + index + " é " + numeros[index]);
+}
+catch (Exception e) when (e is IndexOutOfRangeException || e is ArgumentNullException)
+{
+    Console.WriteLine("\nErro:" + e.Message);
+}
+/* Uma cláusula try-catch é usada para capturar exceções que podem ser lançadas por essa instrução.
+Uma cláusula when é adicionada à cláusula catch para filtrar as exceções que serão capturadas.
+Neste caso, estamos interessados em capturar exceções do tipo IndexOutOfRangeException ou
+ArgumentNullException, que podem ser lançadas quando o índice é inválido ou o array é nulo, respectivamente.
+Se uma exceção IndexOutOfRangeException ou ArgumentNullException for lançada, a cláusula when capturará a
+exceção e exibirá uma mensagem de erro na tela. Se outra exceção for lançada, ela não será capturada e o programa
+encerrará.
+Para testar este exercício, tente alterar o valor do índice index para um valor dentro dos limites do array, defina o
+array como nulo ou adicione mais filtros de exceção para serem capturados pela cláusula when.
+Veja como o programa trata cada situação e se a mensagem apropriada é exibida na tela. */
---
+++ Exercicios_TratamentoErros/Exercicio4/Program.cs
@@ -0,0 +1,33 @@
+﻿
+try
+{
+    int saldo = 00;
+    int valorSaque = 100;
+    if (valorSaque > valorSaque)
+    {
+        throw new SaldoInsuficienteException("O saldo é insuficiente para este saque");
+    }
+    saldo -= valorSaque;
+    Console.WriteLine("Saque efetuado com sucesso. Novo saldo:" + saldo);
+}
+catch (SaldoInsuficienteException e)
+{
+    Console.WriteLine("Erro:" + e.Message);
+}
+class SaldoInsuficienteException : Exception
+{
+    public SaldoInsuficienteException() { }
+    public SaldoInsuficienteException(string message) : base(message) { }
+    public SaldoInsuficienteException(string message, Exception innerException) : base(message, innerException) { }
+}
+/* Neste exercício, uma variável saldo é inicializada com um valor de 0, representando o saldo de uma conta bancária.
+Em seguida, uma variável valorSaque é definida com um valor de 100, que é maior que o saldo atual.
+Uma verificação é realizada para verificar se o valor do saque é maior que o saldo. Se for, uma exceção personalizada
+SaldoInsuficienteException é lançada com a mensagem "O saldo é insuficiente para este saque."
+Uma cláusula try-catch é usada para capturar essa exceção personalizada e exibir uma mensagem de erro na tela.
+A exceção personalizada SaldoInsuficienteException é definida como uma classe separada que herda da classe base
+Exception. Ela contém três construtores: um construtor sem argumentos, um construtor que recebe uma mensagem
+como argumento e um construtor que recebe uma mensagem e uma exceção interna como argumentos.
+Para testar este exercício, tente alterar o valor da variável valorSaque para um valor menor que o saldo, adicione
+mais verificações ou exceções personalizadas, ou defina a variável saldo como nula. Veja como o programa trata cada
+situação e se a mensagem apropriada é exibida na tela. */
