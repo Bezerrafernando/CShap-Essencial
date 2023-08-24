5336c9979d2cf7109bc4e8c4c47b25edc02d3e8c
bezerrafernando <fernandodbezerra@gmail.com>
2023-08-24 15:50:11 -0300

    Learn

---
+++ Delegates_Expressão_Lambda_Eventos_LINQ/11Eventos/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("\nUsando o evento OnCriarPedido");
+
+var pedido = new Pedido();
+
+Console.ReadKey();
+
+//increver os assinantes em evento
+pedido.OnCriarPedido += Email.Enviar;
+pedido.OnCriarPedido += SMS.Enviar;
+
+pedido.CriarPedido();
+
+Console.ReadKey();
+
+class Pedido
+{
+    //criar evento associado ao delegate
+    public event EventHandler? OnCriarPedido;
+    public void CriarPedido()
+    {
+        Console.WriteLine("\nPedido criado !!!");
+
+        if (OnCriarPedido != null)
+        {
+            OnCriarPedido(this, EventArgs.Empty);
+        }
+    }
+}
+class Email
+{
+    public static void Enviar(object? sender, EventArgs e)
+    {
+        Console.WriteLine("\nEnviando um email");
+    }
+}
+class SMS{
+    public static void Enviar(object? sender, EventArgs e){
+        Console.WriteLine("\nEnviar um SMS");
+    }
+}
